cb30180089b95f05963ad56d3d94e8fcaf00ca8e
Justin Diaz <38015681+justinok@users.noreply.github.com>
2023-05-19 07:54:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('Hola viajero 🥣 🥗 🐔 🥑🍞')
 
 streamlit.header('te deseo')
 streamlit.text('un maravilloso dia')
 streamlit.header('🍌🥭 Recuerda alimentarte bien 🥝🍇')
+
+streamlit.dataframe(my_fruit_list)
