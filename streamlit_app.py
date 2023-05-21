f9676d268f24d5700e9e293706868411ce72e32a
Justin Diaz <38015681+justinok@users.noreply.github.com>
2023-05-20 19:10:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
+streamlit.title('Laaaaaa aaaaaaaamooo mamasota 🥣 🥗 🐔 🥑🍞')
 
 
 # write your own comment -what does the next line do? 
