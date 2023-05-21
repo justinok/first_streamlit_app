280f0c0a7430b744236d4435ce7cd8843fdbe56e
Justin Diaz <38015681+justinok@users.noreply.github.com>
2023-05-20 20:51:23 -0500

    function to handle repteable code is implemented

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,14 +20,21 @@ streamlit.dataframe(fruits_to_show)
 #Display API response
 
 streamlit.header("Fruityvice Fruit Advice!")
+
+#Create repeteable block function
+def get_fruitvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+  
+#New selection to display API repsonse
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("please select one fruit to get info")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruitvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
     
 except URLError as e:
   streamlit.error()
